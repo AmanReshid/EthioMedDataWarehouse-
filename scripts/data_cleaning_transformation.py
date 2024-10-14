37953f29b3103c1f9c876e13a23dbe31387e4cae
AmanReshid <amanreshid27@gmail.com>
2024-10-14 09:26:14 +0300

    commit
    commit

--- scripts/data_cleaning_transformation.py
+++ scripts/data_cleaning_transformation.py
@@ -3,6 +3,7 @@
 import os
 import pandas as pd
 import logging
+import emoji
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 
@@ -106,4 +107,33 @@ def dbt_testing_and_docs():
     except Exception as e:
         logging.error(f"Error in DBT testing or documentation: {e}")
 
+def remove_emojis(text):
+    return emoji.replace_emoji(text, replace='')
 
+def summary(df):
+    """Generate a summary of columns in the DataFrame."""
+    summary_data = []
+    
+    for col_name in df.columns:
+        col_dtype = df[col_name].dtype
+        num_of_nulls = df[col_name].isnull().sum()
+        num_of_non_nulls = df[col_name].notnull().sum()
+        num_of_distinct_values = df[col_name].nunique()
+        
+        if num_of_distinct_values <= 10:
+            distinct_values_counts = df[col_name].value_counts().to_dict()
+        else:
+            top_10_values_counts = df[col_name].value_counts().head(10).to_dict()
+            distinct_values_counts = {k: v for k, v in sorted(top_10_values_counts.items(), key=lambda item: item[1], reverse=True)}
+
+        summary_data.append({
+            'col_name': col_name,
+            'col_dtype': col_dtype,
+            'num_of_nulls': num_of_nulls,
+            'num_of_non_nulls': num_of_non_nulls,
+            'num_of_distinct_values': num_of_distinct_values,
+            'distinct_values_counts': distinct_values_counts
+        })
+    
+    df = pd.DataFrame(summary_data)
+    return df
